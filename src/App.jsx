3a574845e586f3448fdfbdbec15b65c368bb6bec
Askhab <akhtaevaskhab07@mail.ru>
2024-04-21 14:54:24 +0300

    new app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/app.css';
+import AsideBar from './components/AsideBar';
+import './styles/app.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <AsideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AsideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AsideBar = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default AsideBar;
